d2bddde53f99bb3b90e0e3fbe7b2e039abe3bef4
DealiAxy <dealiaxy@gmail.com>
2024-08-14 16:42:12 +0800

    🍅安装了croniter才能支持django-q的cron任务

--- src/config/settings/components/django_q.py
+++ src/config/settings/components/django_q.py
@@ -1,28 +1,28 @@
 from config.settings.components.common import DOCKER
 
-# Q_CLUSTER = {
-#     'name': 'today_tv',
-#     'workers': 4,
-#     'recycle': 500,
-#     'timeout': 60,
-#     'compress': True,
-#     'cpu_affinity': 1,
-#     'save_limit': 250,
-#     'queue_limit': 500,
-#     'label': 'Django Q',
-#     'redis': {
-#         'host': 'redis' if DOCKER else 'localhost',
-#         'port': 6379,
-#         'db': 2,
-#     }
-# }
-
 Q_CLUSTER = {
-    'name': 'DjangORM',
+    'name': 'today_tv',
     'workers': 4,
-    'timeout': 90,
-    'retry': 120,
-    'queue_limit': 50,
-    'bulk': 10,
-    'orm': 'qcluster'
+    'recycle': 500,
+    'timeout': 60,
+    'compress': True,
+    'cpu_affinity': 1,
+    'save_limit': 250,
+    'queue_limit': 500,
+    'label': 'Django Q',
+    'redis': {
+        'host': 'redis' if DOCKER else 'localhost',
+        'port': 6379,
+        'db': 2,
+    }
 }
+
+# Q_CLUSTER = {
+#     'name': 'DjangORM',
+#     'workers': 4,
+#     'timeout': 90,
+#     'retry': 120,
+#     'queue_limit': 50,
+#     'bulk': 10,
+#     'orm': 'qcluster'
+# }

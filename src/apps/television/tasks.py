586ef4719b6f5e65e826bc83695f141a0bd56901
DealiAxy <dealiaxy@gmail.com>
2024-09-22 15:42:17 +0800

    🍈怎么还有未提交的

---
+++ src/apps/television/tasks.py
@@ -0,0 +1,22 @@
+from apps.television.models import Video
+from celery import shared_task
+
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
+
+
+@shared_task
+def count_videos():
+    return Video.objects.count()
---
+++ src/config/celery.py
@@ -0,0 +1,23 @@
+import os
+
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('today-tv')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+# 可重用应用程序的常见做法是在单独的tasks.py模块中定义所有任务，并且Celery确实有一种方法可以自动发现这些模块
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
